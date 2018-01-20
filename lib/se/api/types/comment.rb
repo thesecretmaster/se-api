3c210187c5dbc62c28e26202701f7dd914b1dcb9
izwick-schachter <isaiah@developingtechnician.com>
2018-01-20 12:40:23 -0500

    Added Comment#post_type

--- lib/se/api/types/comment.rb
+++ lib/se/api/types/comment.rb
@@ -3,7 +3,7 @@ require "se/api/types/user"
 module SE
   module API
     class Comment
-      attr_reader :body, :link, :id, :post_id, :score, :type, :created_at, :author, :body_markdown, :edited
+      attr_reader :body, :link, :id, :post_id, :score, :type, :created_at, :author, :body_markdown, :edited, :post_type
       attr_reader :json
 
       def initialize(item_json)
@@ -13,6 +13,7 @@ module SE
         @link = @json["link"]
         @edited = (@json["edited"] == "true")
         @post_id = @json["post_id"].to_i
+        @post_type = @json["post_type"]
         @score = @json["score"].to_i
         @type = @json["post_type"]
         @id = @json["comment_id"].to_i
