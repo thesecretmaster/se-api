c89b0e7dd2052c4f63c4f520a8c0961d8042ea96
izwick-schachter <isaiah@developingtechnician.com>
2018-01-20 12:35:05 -0500

    Code cleanup

--- lib/se/api/types/comment.rb
+++ lib/se/api/types/comment.rb
@@ -8,15 +8,15 @@ module SE
 
       def initialize(item_json)
         @json = item_json
-        @body = item_json["body"]
-        @body_markdown = item_json["body_markdown"]
-        @link = item_json["link"]
-        @post_id = item_json["post_id"].to_i
-        @score = item_json["score"].to_i
-        @type = item_json["post_type"]
-        @id = item_json["comment_id"].to_i
-        @created_at = item_json["creation_date"]
-        @author = User.new(item_json["owner"])
+        @body = @json["body"]
+        @body_markdown = @json["body_markdown"]
+        @link = @json["link"]
+        @post_id = @json["post_id"].to_i
+        @score = @json["score"].to_i
+        @type = @json["post_type"]
+        @id = @json["comment_id"].to_i
+        @created_at = @json["creation_date"]
+        @author = User.new(@json["owner"])
       end
 
       alias_method :user, :author
